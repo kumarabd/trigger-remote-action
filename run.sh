87c44819009e17b229c6b0b9a3c53d028def9202
kumarabd <abishekkumar92@gmail.com>
2021-02-15 15:49:15 +0530

    init commit
    Signed-off-by: kumarabd <abishekkumar92@gmail.com>

--- run.sh
+++ run.sh
@@ -2,11 +2,6 @@
 
 GITHUB_API_URL="api.github.com"
 
-if [[ -z "$INPUT_FILE" ]]; then
-  echo "Missing file input in the action"
-  exit 1
-fi
-
 if [[ -z "$GITHUB_REPOSITORY" ]]; then
   echo "Missing GITHUB_REPOSITORY env variable"
   exit 1
@@ -17,15 +12,9 @@ if ! [[ -z ${INPUT_REPO} ]]; then
   REPO=$INPUT_REPO
 fi
 
-TARGET_REPO=$GITHUB_REPOSITORY
-if ! [[ -z ${INPUT_RELEASE_REPO} ]]; then
-  TARGET_REPO=$INPUT_RELEASE_REPO
-fi
-
-# Optional target file path
-TARGET=$INPUT_FILE
-if ! [[ -z ${INPUT_TARGET} ]]; then
-  TARGET=$INPUT_TARGET
+NAME=$INPUT_NAME
+if ! [[ -z ${INPUT_NAME} ]]; then
+  NAME=$INPUT_NAME
 fi
 
 # Optional personal access token for external repository
@@ -34,62 +23,20 @@ if ! [[ -z ${INPUT_TOKEN} ]]; then
   TOKEN=$INPUT_TOKEN
 fi
 
-API_URL="https://$GITHUB_API_URL/repos/$REPO"
-RELEASE_DATA=$(curl -H "Authorization: token $TOKEN" $API_URL/releases?tag_name=${INPUT_VERSION})
-MESSAGE=$(echo $RELEASE_DATA | jq -r ".[].message")
+WORKFLOW_API_URL="https://$GITHUB_API_URL/repos/$REPO/actions/workflows"
+RELEASE_DATA=$(curl -H "Authorization: token $TOKEN" $WORKFLOW_API_URL)
+ACTION_ID=$(echo $RELEASE_DATA | jq -r ".workflows[] | select(.name == \"$NAME\").id")
 
-if [[ "$MESSAGE" == "Not Found" ]]; then
-  echo "[!] Release asset not found"
-  echo "Release data: $RELEASE_DATA"
-  echo "-----"
-  echo "repo: $REPO"
-  echo "asset: $INPUT_FILE"
-  echo "target: $TARGET"
-  echo "version: $INPUT_VERSION"
-  exit 1
-fi
-
-ASSET_ID=$(echo $RELEASE_DATA | jq -r ".[].assets | map(select(.name == \"${INPUT_FILE}\"))[0].id")
-ASSET_TYPE=$(echo $RELEASE_DATA | jq -r ".[].assets | map(select(.name == \"${INPUT_FILE}\"))[0].content_type")
-TAG_VERSION=$(echo $RELEASE_DATA | jq -r ".[].tag_name" | sed -e "s/^v//" | sed -e "s/^v.//")
-
-if [[ -z "$ASSET_ID" ]]; then
-  echo "Could not find asset id"
+if [[ "$ACTION_ID" == "" ]]; then
+  echo "[!] Action not found"
   exit 1
 fi
 
 curl \
-  -J \
-  -L \
-  -H "Authorization: token $TOKEN" \
-  -H "Accept: application/octet-stream" \
-  "$API_URL/releases/assets/$ASSET_ID" \
-  --create-dirs \
-  -o ${INPUT_RELEASE_TARGET}
-
-echo "::set-output name=version::$TAG_VERSION"
-
-# Release process
-TARGET_API_URL="https://$GITHUB_API_URL/repos/$TARGET_REPO"
-RELEASE_URL=$TARGET_API_URL/releases
-
-curl \
   -X POST \
-  -H "Authorization: token $TOKEN" \
+  -H "Authorization: token a4facd4b63f9f6e094ed9797bb1837c023928d01" \
   -H "Accept: application/vnd.github.v3+json" \
-  "$RELEASE_URL" \
-  -d '{"tag_name":"'$INPUT_RELEASE_VERSION'","body":""}'
+  $WORKFLOW_API_URL/$ACTION_ID/dispatches \
+  -d '{"ref":"master","inputs": {"version":"'${INPUT_VERSION}'"}}'
 
-
-# Upload artifact
-RELEASE_UPLOAD_URL=$(curl -H "Authorization: token $TOKEN" $RELEASE_URL?tag_name=${INPUT_RELEASE_VERSION} | jq -r ".[].upload_url")
-pattern="{?"
-RELEASE_ASSET_URL="${RELEASE_UPLOAD_URL%$pattern*}?name=$INPUT_RELEASE_TARGET"
-
-curl \
-  -X POST \
-  -H "Content-Type: $ASSET_TYPE" \
-  -H "Authorization: token $TOKEN" \
-  -H "Accept: application/vnd.github.v3+json" \
-  --data-binary @${INPUT_RELEASE_TARGET} \
-  "$RELEASE_ASSET_URL"
+echo "Triggered: "$ACTION_ID
