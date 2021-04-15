db483f192fe389853d6091b2a82092eae6650fa9
kumarabd <abishekkumar92@gmail.com>
2021-04-15 17:11:08 +0530

    init commit
    Signed-off-by: kumarabd <abishekkumar92@gmail.com>

--- run.sh
+++ run.sh
@@ -23,6 +23,12 @@ if ! [[ -z ${INPUT_TOKEN} ]]; then
   TOKEN=$INPUT_TOKEN
 fi
 
+echo "Fetching latest revision...."
+REVISION_API_URL="https://$GITHUB_API_URL/repos/$REPO/releases"
+LATEST_VERSION=$(curl -H "Authorization: token $TOKEN" $REVISION_API_URL | jq -r '.[0]' | jq -r .tag_name)
+REVISION=$(cut -d '-' -f2 <<<"$LATEST_VERSION")
+echo "Latest revision: $REVISION"
+
 WORKFLOW_API_URL="https://$GITHUB_API_URL/repos/$REPO/actions/workflows"
 RELEASE_DATA=$(curl -H "Authorization: token $TOKEN" $WORKFLOW_API_URL)
 ACTION_ID=$(echo $RELEASE_DATA | jq -r ".workflows[] | select(.name == \"$NAME\").id")
@@ -37,6 +43,6 @@ curl \
   -H "Authorization: token $TOKEN" \
   -H "Accept: application/vnd.github.v3+json" \
   $WORKFLOW_API_URL/$ACTION_ID/dispatches \
-  -d '{"ref":"master","inputs": {"version":"'${INPUT_VERSION}'"}}'
+  -d '{"ref":"master","inputs": {"version":"'${INPUT_VERSION}'","revision":"'${REVISION}'"}}'
 
 echo "Triggered: "$ACTION_ID
